7b9048bd497570bd1e7206867b6510a2d67f2274
Ahmad <ahmi2662@gmail.com>
2026-06-17 21:46:06 +0500

    Fix Relief OS image path for production

--- app/relief-os/page.tsx
+++ app/relief-os/page.tsx
@@ -90,7 +90,7 @@ export default function ReliefOSPage() {
             <div className="relative aspect-[16/8]">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="/Images/Relief Os/screen.png"
+                src="/images/relief-os/screen.png"
                 alt="Relief OS platform dashboard screen"
                 className="h-full w-full object-cover"
               />
